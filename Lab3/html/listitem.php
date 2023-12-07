18a9e413444682f23989a5820e5e4d2dd1a4d52b
Pong110347 <phichet27626@gmail.com>
2023-12-07 15:56:05 +0700

    Lab5

--- Lab3/html/listitem.php
+++ Lab3/html/listitem.php
@@ -7,10 +7,10 @@ $result = mysqli_query($conn, $sql);
 // var_dump($result);
 ?>
 
-<button id = "btn_add">+ Add</button>
+<button id="btn_add" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">+ Add</button>
 
 
-<table>
+<table class="table table-striped table-hover">
     <thead>
         <tr>
             <th>ID</th>
@@ -21,14 +21,15 @@ $result = mysqli_query($conn, $sql);
     </thead>
     <tbody>
         <?php
-        while($row = mysqli_fetch_assoc($result)) {
+        while ($row = mysqli_fetch_assoc($result)) {
         ?>
-        <tr>
-            <td><?= $row["id_member"] ?></td>
-            <td><?= $row["name"] ?></td>
-            <td><?= $row["id_province"] ?></td>
-            <td><button class="btn_del" data-id="<?= $row["id_member"]?> "> DEl </button></td>
-        </tr>
+            <tr>
+                <td><?= $row["id_member"] ?></td>
+                <td><?= $row["name"] ?></td>
+                <td><?= $row["id_province"] ?></td>
+                <td><button class="btn_del" data-id="<?= $row["id_member"] ?>"> DEl </button></td>
+                <td><button class="btn_edt" data-id="<?= $row["id_member"] ?>" data-bs-toggle="modal" data-bs-target="#staticBackdrop"> Edit </button></td>
+            </tr>
         <?php
         }
         ?>
@@ -36,28 +37,39 @@ $result = mysqli_query($conn, $sql);
 </table>
 
 <script>
-    $(".btn_del").click(function(){
+    $(".btn_del").click(function() {
         let id = $(this).data("id");
         console.log(id);
 
         $.ajax({
-            url:"/delitem.php",
-            method:"GET",
-            data:{
+            url: "/delitem.php",
+            method: "GET",
+            data: {
                 id_mem: id
             },
             success: function(res) {
                 console.log(res);
-                if(res == "error")
+                if (res == "error")
                     alert("con't delete item.");
                 else
                     $("#div_item").load("/listitem.php");
-           } 
+            }
         });
     });
 
-    $("#btn_add").click(function(){
-        $("#div_item").load("/addform.php");
+    $("#btn_add").click(function() {
+        //$("#div_item").load("/addform.php");
+        $("#staticBackdropLabel").text("Add Item");
+        $(".modal-body").load("/addform.php");
+        $(".modal-footer").hide();
     });
 
+    $(".btn_edt").click(function() {
+        let id = $(this).data("id");
+
+        //$("#div_item").load("/Editform.php");
+        $("#staticBackdropLabel").text("Edit Item");
+        $(".modal-body").load(`/Editform.php?id=${id}`);
+        $(".modal-footer").hide();
+    });
 </script>
